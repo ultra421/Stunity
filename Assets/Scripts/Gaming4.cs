93bc6fe6f9f78036135ebcca13524878e2725428
David Elias <delias200603@gmail.com>
2022-12-15 18:16:46 +0100

    15-12-2022

--- Assets/Scripts/Gaming4.cs
+++ Assets/Scripts/Gaming4.cs
@@ -6,21 +6,27 @@ public class Gaming4 : MonoBehaviour
 {
 
     Rigidbody2D rb;
-    [SerializeField] Vector2 sideSpeed;
-    [SerializeField] Vector2 jumpSpeed;
-    bool jumpInput;
-    bool isGround;
-    [SerializeField] Vector2 vel,maxVel;
+    [SerializeField] Vector2 sideSpeed, jumpSpeed, vel, maxVel;
+    bool jumpInput, isGround, isJumping;
+    [SerializeField] int maxJumps;
+    int jumps;
+    [SerializeField] int downForce;
+    float timeSinceJumpInput;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         vel = rb.velocity;
+        //Defaults
         maxVel = new Vector2(5, 100);
         sideSpeed = new Vector2(20,0);
-        jumpSpeed = new Vector2(0,500);
+        jumpSpeed = new Vector2(0,350);
         jumpInput = false;
+        maxJumps = 2;
+        downForce = 25;
+        timeSinceJumpInput = 0;
+        //
         isGround = false;
     }
 
@@ -35,6 +41,8 @@ public class Gaming4 : MonoBehaviour
         vel = rb.velocity;
         CheckGround();
         Movement();
+        //FINAL
+        FinalChecks();
     }
 
     private void CheckInputs()
@@ -49,6 +57,8 @@ public class Gaming4 : MonoBehaviour
     {
         InitialChecks();
         CheckMaxSpeed();
+        StopHorizontalSpeed();
+        MultiplyVerticalHoldingJump();
     }
 
     private void InitialChecks()
@@ -74,11 +84,15 @@ public class Gaming4 : MonoBehaviour
             rb.AddForce(sideSpeed);
         }
 
-        if (jumpInput && isGround)
+        //If jumpInput and is ground or in air and not max jumps
+        if (jumpInput && (isGround || (!isGround && jumps < maxJumps)))
         {
-            Debug.Log("jumpin");
+            //Reset vertical velocity
+            vel.y = 0;
             rb.AddForce(jumpSpeed);
             jumpInput = false;
+            jumps++;
+            Debug.Log("jumpin / jumps = " + jumps);
         }
     }
     private void CheckMaxSpeed()
@@ -93,8 +107,6 @@ public class Gaming4 : MonoBehaviour
         {
             vel.x = -maxVel.x;
         }
-        //TODO: Remove
-        rb.velocity = vel;
     }
 
     private void CheckGround()
@@ -102,7 +114,7 @@ public class Gaming4 : MonoBehaviour
         //Start ray below player
         Vector2 pos = rb.position;
         BoxCollider2D bc2 = GetComponent<BoxCollider2D>();
-        float rayDistance = 0.16f;
+        float rayDistance = 0.1f;
         Vector2 rayStart = new Vector2(pos.x, pos.y - bc2.bounds.extents.y-0.01f);
       
         RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down,rayDistance);
@@ -123,6 +135,79 @@ public class Gaming4 : MonoBehaviour
                 Debug.Log("touched ground: " + collided);
             }
             isGround = true;
+            //If going up don't reset jumps
+            if (!(vel.y > 0) || IsNearZero(vel.y))
+            {
+                jumps = 0;
+            }
+        }
+
+    }
+
+    private void FinalChecks()
+    {
+        //Assign modified vel to rb
+        Debug.Log("Initial vel = " + rb.velocity + " result: " + vel);
+        rb.velocity = vel;
+    }
+    
+    //Additional force applied depends on if holding jump or not
+    private void MultiplyVerticalHoldingJump()
+    {
+        if (!isGround)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(new Vector2(0, -downForce * 0.25f));
+            } else
+            {
+                rb.AddForce(new Vector2(0,-downForce));
+            }
+        }
+    }
+
+    private bool IsNearZero(float input)
+    {
+        if ((input < 0.25f && input > -0.25f) && input != 0)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    private void StopHorizontalSpeed()
+    {
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+
+            if (IsNearZero(vel.x))
+            {
+                Debug.Log("Speed is near 0");
+                vel.x = 0;
+                return;
+            }
+
+            //First check if on ground or air, change multilpier depending on that
+            float slowDownSpeed;
+            if (isGround) // Ground
+            {
+                slowDownSpeed = sideSpeed.x * 0.8f;
+            } else // Air
+            {
+                slowDownSpeed = sideSpeed.x * 0.4f;
+            }
+
+            if (vel.x > 0) //Going right
+            {
+                rb.AddForce(new Vector2(-slowDownSpeed,0));
+            }
+            else if (vel.x < 0) //Going left
+            {
+                rb.AddForce(new Vector2(slowDownSpeed,0));
+            }
         }
 
     }
